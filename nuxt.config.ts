af07a8e52a171166f970720b6990e4b33023d6a8
Kapil Chaudhary <kc685838@gmail.com>
2024-01-18 09:28:01 +0530

    Fixed: Missing .css issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["@builder.io/sdk-vue/vue3/css"],
+  build: {
+    transpile: ['@builder.io/sdk-vue']
+  },
   runtimeConfig: {
     public: {
       builderApiKey: "",
